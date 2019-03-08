2a9ce93764a2a197c5769757dbee0a7d569fa517
chakal05 <chakirrasta@gmail.com>
2019-03-08 20:42:29 +0100

    upload and display uploaded files

--- mesDocuments.php
+++ mesDocuments.php
@@ -1,38 +1,77 @@
 <?php
 session_start();
-
+require_once("connection.php");
 if(!isset($_SESSION['id'])){
     header("location: index.php");
+}else{
+    $user = $_SESSION['id'];
 }
+
 
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
+    <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>mes docs</title>
+    <title>Messages</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="mesDocuments.css">
-    <script src=""></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" media="screen" href="mesdocuments.css">
+
+    
 </head>
 <body>
-<?php include('header.php');  ?>
 
-    <div class="container">
-    <div class="left">
-    <p>mes documents </p>
-    </div>
-    <div class="right">
-    <form action="upload.php" method="post" enctype="multipart/form-data">
-    Select image to upload:
-    <input type="file" name="fileToUpload" >
-    <input type="submit" name="submit" value="upload">
-</form>
-    </div>
-   
-    </div>
+<div>
+<?php include("header.php"); ?>
+</div>
 
+<div class="container">
+
+<div class="left">
+<h3>Mes documents</h3>
+    <?php 
+$sql = "SELECT * FROM `uloaded` WHERE user='$user'";
+$result = mysqli_query($link,$sql);
+//check their are any messages
+if(mysqli_num_rows($result) > 0){
+
+    // format and display
+
+    while($row= mysqli_fetch_array($result)){
+        $fileName = $row["file_name"];
+        $path= $row['file_path'];
+        echo "<a href='uploads/{$path}'>$fileName</a><br>";
+    }
+
+}
+
+
+
+ ?>
+
+</div>
+
+<div class="right">
+    <ul>
+        <li><a href="linkToUpload.html">charger un document</a></li>
+        <li><a href="#">supprimer un document </a></li>
+        <li><a href="#">telecharger un document </a></li>
+    </ul>
+</div>
+
+
+</div>
+
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+ <script src="jquery.min.js"></script>
+ <script src="script.js"></script>
 </body>
 </html>
--- upload.php
+++ upload.php
@@ -1,8 +1,86 @@
 <?php
 session_start();
+require_once("connection.php");
 
-if(isset($_FILES['fileToUpload'])){
-    $file = $_FILES['fileToUpload'];
-    print_r($file);
+if(!isset($_SESSION['id'])){
+    header("location: index.php");
+}else{
+    $user= $_SESSION['id'];
 }
+
+$target_dir = "uploads/";
+$file = $_FILES["fileToUpload"];
+$target_file = $target_dir . basename($file["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+$allowedMimeTypes = array( 
+    'application/msword',
+    'text/pdf',
+    'text/doc',
+    'text/docx',
+    'text/txt'
+  );
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($file["tmp_name"]);
+    if($check !== false) {
+       echo "File is an image - " . $check["mime"] . "."."<br>";
+        $uploadOk = 1;
+    } else if(in_array( $file["type"], $allowedMimeTypes ) ) {
+        echo "File is text document.";
+        $uploadOk = 1;
+    }else{
+        $uploadOk= 0;
+    }
+}
+// Check if file already exists
+
+// Check file size
+if ($file["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" && $imageFileType != "pdf" && $imageFileType != "txt" && $imageFileType != "doc" && $imageFileType != "docx" ) {
+    echo "Sorry, only JPG, JPEG, PNG, PDF, TXT, DOC, DOCX & GIF files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    // Change the name of the file to avoid conflicts of names in database 
+    $cleanName = str_replace(' ','_',$file['name']);
+    $date = date_create();
+    $timest = date_timestamp_get($date);
+    $pathName = $timest.$cleanName;
+    $name = $file["name"];
+    $target_file = $target_dir.$pathName;
+    if (move_uploaded_file($file["tmp_name"], $target_file)) {
+        echo "The file ". basename( $file["name"]). " has been uploaded.<br>";
+        echo "name in database is ".$pathName."<br>";
+        // Insert in database
+        $sql = "INSERT INTO `uloaded` VALUES ('','$user','$name','$pathName')";
+        $query= mysqli_query($link,$sql);
+        
+        if($query){
+
+            //insert was successful
+            echo "Posted";
+
+           } else {
+           
+              //insert failed
+              echo "Error";
+           
+           }
+       echo "<a href='mesDocuments.php'>Go back</a>";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+
+
 ?>
