e5ae29753398f6448df33ca9e45817e2ae629b83
chakal05 <chakirrasta@gmail.com>
2019-03-16 16:53:14 +0100

    new css for index.php

--- index.php
+++ index.php
@@ -75,6 +75,7 @@ session_start();
     <title>Lycée de Balbala</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" media="screen" href="index.css">
 
     
@@ -83,15 +84,15 @@ session_start();
    
     
 
-<div class="content">
-    <div class="logo">
-   <img src="logo_devise.jpg">
-   </div>
 
-     </div>
+
+     
    
      <div class= "container">
       
+    <div class="logo">
+   <img src="logo_devise.jpg">
+   </div>
 
     <h1> Université de Djibouti</h1>
     
